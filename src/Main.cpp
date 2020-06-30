fef20b6841cc90734a007e7d7cb104d332200a05
offa <bm-dev@yandex.com>
2020-06-30 18:48:33 +0200

    Basic CLI added.

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,5 +1,17 @@
+#include <argparse/argparse.hpp>
 
-int main()
+int main(int argc, char* argv[])
 {
+    argparse::ArgumentParser argParser{"cet"};
+    argParser.add_argument("config");
+
+    try
+    {
+        argParser.parse_args(argc, argv);
+    }
+    catch (const std::runtime_error& ex)
+    {
+        std::cout << argParser;
+    }
     return 0;
 }

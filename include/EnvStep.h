33ec3c2702e6018bc995a6f00da105cd854087d8
offa <bm-dev@yandex.com>
2021-01-05 13:37:49 +0100

    Year updated.

--- include/EnvStep.h
+++ include/EnvStep.h
@@ -1,7 +1,7 @@
 /*
  * cet - Container Environment Test
  *
- * Copyright (C) 2020  offa
+ * Copyright (C) 2020-2021  offa
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
